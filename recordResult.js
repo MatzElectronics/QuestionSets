2df63c68e6cdb6abac9d8227bafb2687f92d3dee
Matthew Matz <19737272+MatzElectronics@users.noreply.github.com>
2020-09-24 12:23:18 -0700

    Update recordResult.js

--- recordResult.js
+++ recordResult.js
@@ -7,19 +7,58 @@ function initSheetsApi () { //initialize the Google API
     gapi.load('client:auth2', initClient);
   }
 
-function initClient () { //provide the authentication credentials you set up in the Google developer console
-    gapi.client.init({
-      'apiKey': API_KEY,
-      'clientId': CLIENT_ID,
-      'scope': SCOPE,
-      'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-    })
-    
-    //.then(()=> {
-    //  gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus); //add a function called `updateSignInStatus` if you want to do something once a user is logged in with Google
-    //  updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    //});
+var GoogleAuth; // Google Auth object.
+function initClient() {
+  gapi.client.init({
+      'apiKey': 'YOUR_API_KEY',
+      'clientId': 'YOUR_CLIENT_ID',
+      'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+      'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest']
+  }).then(function () {
+      GoogleAuth = gapi.auth2.getAuthInstance();
+
+      // Listen for sign-in state changes.
+      GoogleAuth.isSignedIn.listen(updateSigninStatus);
+  });
+}
+
+var isAuthorized;
+var currentApiRequest;
+
+/**
+ * Store the request details. Then check to determine whether the user
+ * has authorized the application.
+ *   - If the user has granted access, make the API request.
+ *   - If the user has not granted access, initiate the sign-in flow.
+ */
+function sendAuthorizedApiRequest(requestDetails) {
+  currentApiRequest = requestDetails;
+  if (isAuthorized) {
+    // Make API request
+    // gapi.client.request(requestDetails)
+
+    // Reset currentApiRequest variable.
+    currentApiRequest = {};
+  } else {
+    GoogleAuth.signIn();
   }
+}
+
+/**
+ * Listener called when user completes auth flow. If the currentApiRequest
+ * variable is set, then the user was prompted to authorize the application
+ * before the request executed. In that case, proceed with that API request.
+ */
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    isAuthorized = true;
+    if (currentApiRequest) {
+      sendAuthorizedApiRequest(currentApiRequest);
+    }
+  } else {
+    isAuthorized = false;
+  }
+}
 
  function onFormSubmit(submissionValues) {
 
